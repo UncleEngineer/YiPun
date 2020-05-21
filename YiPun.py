1fc1af740fd790826d1228e64d3b5f45ba4a1b2d
kidkrub <kritsadamoungkhao@gmail.com>
2020-05-21 19:36:49 +0700

    ADD thai/english text to speech
    เพิ่มอ่านออกเสียงตามคำแปลภาษาไทย/อังกฤษ ปิดใช้งานปุ่มอ่านออกเสียงเมื่อยังไม่ได้แปลภาษา

--- YiPun.py
+++ YiPun.py
@@ -225,8 +225,10 @@ def SpeakNow2(event=None):
 
 	if v_radio.get() == 'ja':
 		tts = gTTS(text=v_transvocab.get(), lang='ja')
+	elif v_radio.get() == 'th':
+		tts = gTTS(text=v_transvocab.get(), lang='th')
 	else:
-		tts = gTTS(text=v_texttras.get(), lang='ja')
+		tts = gTTS(text=v_transvocab.get(), lang='en')
 	
 	if playagain == True:
 		name = generatename()
@@ -492,7 +494,7 @@ v_jisho = StringVar()
 
 def TranslateNow(event=None):
 	print(v_radio.get(), v_texttras.get())
-
+	EB2.config(state='normal') # ทำให้ปุ่มอ่านออกเสียงกลับมากดได้
 	trans = Lam.translate(v_texttras.get(),dest=v_radio.get())
 	v_jisho.set(trans.text)
 	alltext = ''
@@ -555,6 +557,11 @@ def ShowTranslateManual(event=None):
 		LM2.grid(row=0,column=0)
 		E2.grid(row=1,column=0)
 
+# ทำให้ปุ่มกดอ่านออกเสียงกดไม่ได้
+def disabletexttospeech(event=None):
+	EB2.config(state='disabled')
+	print('pressing')
+
 
 v_radio2 = StringVar()
 
@@ -577,6 +584,7 @@ cbtn.pack()
 v_texttras = StringVar() #เก็บสิ่งที่เราพิมพ์ไว้
 E1 = ttk.Entry(F3, textvariable = v_texttras,font=('Angsana New',20),width=70)
 E1.pack(pady=10)
+E1.bind('<KeyPress>', disabletexttospeech)
 E1.bind('<Return>',TranslateNow)
 
 FLM = Frame(F3)
